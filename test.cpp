84d916cd87099dd7a92aa19c74083fa756710fe8
rakeebul <mdrakibulhassan-2023415986@cs.du.ac.bd>
2025-08-22 19:49:01 +0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void turn (int t)
+{
+    for(int i=0;i<t;i++)
+        cout<<"DFGH"<<endl;
+    return;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    cout<<t<<endl;
+    double f;
+printf("%d ", sizeof f);
+    //turn(5);
+
+    return 0;
+}
